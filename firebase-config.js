ea1589c6ee2cf857801ab0e0cf5967e3f5d97933
humbertofb <98710153+humbertofb@users.noreply.github.com>
2025-04-11 16:54:45 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBtf--tWrqRVItAGJGQ6hEk81iggP4I-SU",
+  authDomain: "my-humber-project-319815.firebaseapp.com",
+  projectId: "my-humber-project-319815",
+  storageBucket: "my-humber-project-319815.appspot.com",
+  messagingSenderId: "110994378936",
+  appId: "1:110994378936:web:60b20c16ce5f40b47e644d",
+};
